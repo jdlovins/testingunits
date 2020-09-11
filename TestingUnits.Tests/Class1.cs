b9ebdeedfa88f76577250b6da4c4c56516b0d2ef
Josh Lovins <12jdlovins@gmail.com>
2020-09-11 14:49:10 -0400

    first

---
+++ TestingUnits.Tests/Class1.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestingUnits.Tests
+{
+    [TestFixture]
+    public class CalcTest
+    {
+        private Calculator _calc;
+        public CalcTest()
+        {
+            _calc = new Calculator();
+        }
+
+        [Test]
+        public void CanMath_ReturnTrue()
+        {
+            var result = _calc.Sum(5, 5);
+
+            Assert.AreEqual(result, 10);
+        }
+    }
+}
---
+++ TestingUnits/Calculator.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TestingUnits
+{
+    public class Calculator
+    {
+        public int Sum(int x, int y) => x + y;
+    }
+}
